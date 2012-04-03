9ddf8e8e168945384f27e6bd4d3416fc79d2bd46
Keith Walton <Keith@Walton.net>
2012-04-03 12:38:02 -0700

    Fix deleted project

---
+++ urbanAskTest/UrbanAskTest/Unit Tests/FacebookIntegration.vb
@@ -0,0 +1,140 @@
+﻿#Region " options "
+
+Option Explicit On
+Option Strict On
+Option Compare Binary
+
+#End Region
+
+#Region " imports "
+
+Imports System
+Imports Microsoft.VisualStudio.TestTools
+Imports OpenQA
+Imports OpenQA.Selenium
+Imports UrbanAsk.Test.UrbanAskTest.Utility
+Imports Utility
+
+#End Region
+
+<UnitTesting.TestClass()>
+Public Class FacebookIntegration
+
+#Region "    variables "
+
+    Private _testContext As UnitTesting.TestContext
+    Private _culture As Globalization.CultureInfo = Globalization.CultureInfo.CurrentCulture()
+
+#End Region
+
+#Region "    functions "
+
+#Region "    initialize "
+
+    <UnitTesting.ClassInitialize()>
+    Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+
+    End Sub
+
+    <UnitTesting.TestInitialize()>
+    Public Sub MyTestInitialize()
+
+    End Sub
+
+#End Region
+
+#Region "    tests "
+
+    <UnitTesting.TestMethod()>
+    Public Sub TestMethod1()
+
+        WebDriverUtility.NavigateTo(New System.Uri("http://facebook.com"), "Facebook")
+
+        If WebDriverUtility.IsElementPresent("email") Then
+
+            'TODO: put in .config
+            Dim email As String = "facebooktest1@urbanask.com"
+            Dim password As String = "hooba%stank1"
+            Dim urbanAskFacebookID As String = "267603823260704"
+            Dim applicationsPage As String = "https://www.facebook.com/settings/?tab=applications"
+            Dim urbanAskPermissionPage As String = "http://www.facebook.com/connect/uiserver.php?app_id=267603823260704&method=permissions.request&redirect_uri=http%3A%2F%2Fapps.facebook.com%2Furbanask%2F%3Fref%3Dts&response_type=none&display=page&perms=email%2Cpublish_stream%2Cpublish_actions&auth_referral=1"
+
+            'not logged in
+            WebDriverUtility.Type("email", email)
+            WebDriverUtility.TypeAndEnter("pass", password)
+            WebDriverUtility.Click("loginbutton")
+            WebDriverUtility.WaitForTitle("Facebook")
+            WebDriverUtility.NavigateTo(New System.Uri(applicationsPage), "App Settings")
+
+            'remove ua if it exists
+            Dim elementID As String = String.Format(_culture, "application-li-{0}", urbanAskFacebookID)
+
+            If WebDriverUtility.IsElementPresent(elementID) Then
+
+                WebDriverUtility.Click(By.CssSelector(String.Format(_culture, "#{0} .fbsettingslistitemedit", elementID)))
+                WebDriverUtility.Click(By.ClassName("fbSettingsExpandedDelete"))
+                WebDriverUtility.Click(By.Name("remove"))
+                Threading.Thread.Sleep(1000)
+                WebDriverUtility.AssertElementNotPresent(elementID)
+
+            End If
+
+            WebDriverUtility.NavigateTo(New System.Uri(urbanAskPermissionPage), "Facebook")
+            WebDriverUtility.Click("grant_required_clicked")
+            WebDriverUtility.WaitForBodyText("urbanAsk would also like permission to")
+            WebDriverUtility.Click("grant_clicked")
+            WebDriverUtility.WaitForTitle("urbanAsk on Facebook")
+
+        ElseIf WebDriverUtility.IsElementPresent("userNavigationLabel") Then
+
+            'logged in
+            WebDriverUtility.Click("userNavigationLabel")
+
+        Else
+
+            Assert.Fail("Unknown Facebook page")
+
+        End If
+
+
+    End Sub
+
+#End Region
+
+#Region "    cleanup "
+
+    <UnitTesting.ClassCleanup()>
+    Public Shared Sub MyClassCleanup()
+
+    End Sub
+
+    <UnitTesting.TestCleanup()>
+    Public Sub MyTestCleanup()
+
+    End Sub
+
+#End Region
+
+#End Region
+
+#Region "    properties "
+
+    Public Property TestContext() As TestContext
+
+        Get
+
+            Return _testContext
+
+        End Get
+
+        Set(ByVal value As UnitTesting.TestContext)
+
+            _testContext = value
+
+        End Set
+
+    End Property
+
+#End Region
+
+End Class
---
+++ urbanAskTest/WebDriverUtility/WebDriverUtility.vb
@@ -0,0 +1,305 @@
+﻿#Region " options "
+
+Option Explicit On
+Option Strict On
+Option Compare Binary
+
+#End Region
+
+#Region " imports "
+
+Imports System
+Imports OpenQA
+Imports OpenQA.Selenium
+Imports Microsoft.VisualStudio.TestTools
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+#End Region
+
+
+<Assembly: System.CLSCompliant(True)> 
+Public Class WebDriverUtility
+
+#Region "    constants "
+
+    Private _timeout As New System.TimeSpan(0, 0, 5) '5 seconds
+    Private _sleep As New System.TimeSpan(0, 0, 0, 0, 100) '.1 seconds
+
+#End Region
+
+#Region "    variables "
+
+    Private _driver As OpenQA.Selenium.IWebDriver
+    Private _start As DateTime
+    Private _culture As Globalization.CultureInfo = Globalization.CultureInfo.CurrentCulture()
+
+#End Region
+
+#Region "    functions "
+
+    Public Sub New( _
+       ByVal driver As OpenQA.Selenium.IWebDriver)
+
+        _driver = driver
+
+    End Sub
+
+    Public Overloads Sub NavigateTo(
+        ByVal uri As System.Uri)
+
+        _driver.Navigate().GoToUrl(uri)
+
+    End Sub
+
+    Public Overloads Sub NavigateTo(
+        ByVal uri As System.Uri,
+        ByVal title As String)
+
+        _driver.Navigate().GoToUrl(uri)
+        Me.AssertTitleContains(title)
+
+    End Sub
+
+    Public Overloads Sub AssertElementPresent(
+       ByVal id As String)
+
+        Me.AssertElementPresent(By.Id(id))
+
+    End Sub
+
+    Public Overloads Sub AssertElementPresent(
+        ByVal by As OpenQA.Selenium.By)
+
+        Dim elements As ObjectModel.ReadOnlyCollection(Of OpenQA.Selenium.IWebElement) = _driver.FindElements(by)
+
+        If (elements.Count = 0) Then
+
+            Dim message As String = "Element {0} is not present"
+            UnitTesting.Assert.Fail(String.Format(_culture, message, by))
+
+        End If
+
+    End Sub
+
+    Public Overloads Function IsElementPresent(
+        ByVal id As String) As Boolean
+
+        Return Me.IsElementPresent(By.Id(id))
+
+    End Function
+
+    Public Overloads Function IsElementPresent(
+        ByVal by As OpenQA.Selenium.By) As Boolean
+
+        Dim returnValue As Boolean = False
+
+        Try
+
+            _driver.FindElement(by)
+            returnValue = True
+
+        Catch exception As Selenium.NoSuchElementException
+
+            'not present
+
+        End Try
+
+        Return returnValue
+
+    End Function
+
+    Public Overloads Function WaitForElementPresent(
+        ByVal id As String) As Selenium.IWebElement
+
+        Return Me.WaitForElementPresent(By.Id(id))
+
+    End Function
+
+    Public Overloads Function WaitForElementPresent(
+        ByVal by As OpenQA.Selenium.By) As Selenium.IWebElement
+
+        Dim returnValue As Selenium.IWebElement = Nothing
+
+        Me.ResetTimeout()
+
+        Do Until (Me.Timeout Or (returnValue IsNot Nothing))
+
+            Try
+
+                returnValue = _driver.FindElement(by)
+
+            Catch exception As Selenium.NoSuchElementException
+
+                returnValue = Nothing
+
+            End Try
+
+        Loop
+
+        If (returnValue Is Nothing) Then
+
+            Dim message As String = "Element {0} is not present"
+            UnitTesting.Assert.Fail(String.Format(_culture, message, by))
+
+        End If
+
+        Return returnValue
+
+    End Function
+
+    Public Overloads Sub Click(
+        ByVal id As String)
+
+        Me.Click(By.Id(id))
+
+    End Sub
+
+    Public Overloads Sub Click(
+        ByVal by As OpenQA.Selenium.By)
+
+        Me.WaitForElementPresent(by).Click()
+
+    End Sub
+
+    Public Overloads Sub Type(
+        ByVal id As String, _
+        ByVal text As String)
+
+        Me.Type(By.Id(id), text)
+
+    End Sub
+
+    Public Overloads Sub Type(
+        ByVal by As OpenQA.Selenium.By, _
+        ByVal text As String)
+
+        Dim element As OpenQA.Selenium.IWebElement = WaitForElementPresent(by)
+        element.Clear()
+        element.SendKeys(text)
+
+    End Sub
+
+    Public Overloads Sub TypeAndEnter(
+        ByVal id As String, _
+        ByVal text As String)
+
+        Me.Type(By.Id(id), text)
+
+    End Sub
+
+    Public Overloads Sub TypeAndEnter(
+        ByVal by As OpenQA.Selenium.By, _
+        ByVal text As String)
+
+        Dim element As OpenQA.Selenium.IWebElement = WaitForElementPresent(by)
+        element.SendKeys(text)
+        element.SendKeys(Keys.Enter)
+
+    End Sub
+
+    Public Sub AssertTitle(
+        ByVal title As String)
+
+        Dim message As String = String.Format(_culture, "Title does not match '{0}'", title)
+        UnitTesting.Assert.AreEqual(title, _driver.Title, message)
+
+    End Sub
+
+    Public Sub AssertTitleContains(
+        ByVal text As String)
+
+        If (text IsNot Nothing) _
+            AndAlso (Not _driver.Title.ToLower(_culture).Contains(text.ToLower(_culture))) Then
+
+            Dim message As String = String.Format(_culture, "Title does not contain '{0}'", text)
+            UnitTesting.Assert.Fail(message)
+
+        End If
+
+    End Sub
+
+    Public Overloads Sub AssertElementNotPresent(
+        ByVal id As String)
+
+        Me.AssertElementNotPresent(By.Id(id))
+
+    End Sub
+
+    Public Overloads Sub AssertElementNotPresent(
+        ByVal by As OpenQA.Selenium.By)
+
+        Dim elements As ObjectModel.ReadOnlyCollection(Of OpenQA.Selenium.IWebElement) = _driver.FindElements(by)
+
+        If (elements.Count > 0) Then
+
+            Dim message As String = "Element {0} not present"
+            UnitTesting.Assert.Fail(String.Format(_culture, message, by))
+
+        End If
+
+    End Sub
+
+    Public Sub WaitForTitle(
+        ByVal title As String)
+
+        Me.ResetTimeout()
+        Dim valid As Boolean = False
+
+        Do Until (Me.Timeout Or valid)
+
+            valid = (_driver.Title = title)
+
+        Loop
+
+        If (Not valid) Then
+
+            Me.AssertTitle(title)
+
+        End If
+
+    End Sub
+
+    Public Sub WaitForBodyText(
+        ByVal text As String)
+
+        Me.ResetTimeout()
+
+        Dim xpath As String = "*[contains(.,""{0}"")]"
+        xpath = String.Format(_culture, xpath, text)
+        Dim elements As ObjectModel.ReadOnlyCollection(Of OpenQA.Selenium.IWebElement) = Nothing
+
+        Dim valid As Boolean = False
+
+        Do Until (Me.Timeout Or valid)
+
+            elements = _driver.FindElements(By.XPath(xpath))
+            valid = elements.Count <> 0
+
+        Loop
+
+        Assert.AreEqual(valid, True, String.Format(_culture, "Body text not found: ""{0}"".", text))
+
+    End Sub
+
+    Private Sub Sleep()
+
+        Threading.Thread.Sleep(_sleep)
+
+    End Sub
+
+    Private Sub ResetTimeout()
+
+        _start = System.DateTime.Now
+
+    End Sub
+
+    Private Function Timeout() As Boolean
+
+        Me.Sleep()
+        Return ((System.DateTime.Now() - _start) > _timeout)
+
+    End Function
+
+#End Region
+
+End Class
